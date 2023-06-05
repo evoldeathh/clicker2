037f2fef9f01877b465064dc9707cbc1e0fbf640
evoldeathh <tarstan01@mail.ru>
2023-06-06 01:45:14 +0500

    базовый функционал кликера

--- myproject/backend/views.py
+++ myproject/backend/views.py
@@ -3,13 +3,13 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import CoreSerializer
-
+#
 
 @api_view(['GET'])
 def call_click(request):
-    coreModel = apps.get_model('backend', 'Core')
+    coreModel = apps.get_model('backend', 'core')
     core = coreModel.objects.get(user=request.user)
     core.click()
     core.save()
 
-    return Response({'Core': CoreSerializer(core).data})
+    return Response({'core': CoreSerializer(core).data})
--- myproject/frontend/static/js/index.js
+++ myproject/frontend/static/js/index.js
@@ -1,12 +1,12 @@
 function call_click() {
-    fetch('/call_click',{
+    fetch('/call_click', {
         method: 'GET'
     }).then(response => {
         if (response.ok) {
             return response.json()
         }
         return Promise.reject(response)
-    }).then(data => {
+    }).then(data => {console.log(data);
         document.getElementById('coins').innerText = data.core.coins
     }).catch(error => console.log(error))
 }
